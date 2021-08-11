b884366d47cf78ea6185f127dee505af192d72af
Steven Van Ingelgem <steven@vaningelgem.be>
2021-08-11 23:38:22 +0200

    Detection routines are ok now. Finding the window is ok too. Next would be to implement the logic of the game.

--- src/summoners_greed_bot/detect_gem.py
+++ src/summoners_greed_bot/detect_gem.py
@@ -1,3 +1,4 @@
+from abc import ABC
 from pathlib import Path
 from time import time
 
@@ -5,47 +6,128 @@ import cv2
 from subimage import find_subimages
 
 
-class DetectGem:
+class Detector(ABC):
+    IMAGE_TO_FIND = None
+    counter = 0
+
     def __init__(self):
-        self.visible = cv2.imread('resources/gem_icon_visible.png', 0)
-        self.nope = cv2.imread('resources/gem_icon_not_present.png', 0)
+        self.image_to_find = cv2.imread(str(self.IMAGE_TO_FIND), cv2.IMREAD_GRAYSCALE)
+        self._last_width_height = None
 
-    def is_present(self, img):
-        img = cv2.imread(str(img), 0)
+    @property
+    def last_width_height(self):
+        return self._last_width_height
 
-        # Top right quadrant
-        img = img[:img.shape[0]//2, img.shape[1]//2:]
+    def _rescale(self, h, w):
+        return 0, h, 0, w
 
-        return find_subimages(img, self.visible)
+    def is_present(self, inside_this_image):
+        img = cv2.imread(str(inside_this_image), cv2.IMREAD_GRAYSCALE)
+
+        self._last_width_height = img.shape
+
+        h, w = self._last_width_height
+        tpl = self._rescale(h, w)
+
+        if not isinstance(tpl, tuple):
+            tpl = (tpl, h, 0, w)
+        elif len(tpl) == 1:
+            tpl = (tpl[0], h, 0, w)
+        elif len(tpl) == 2:
+            tpl = (*tpl, 0, w)
+        elif len(tpl) == 3:
+            tpl = (*tpl, w)
+
+        height_slice = slice(tpl[0], tpl[1])
+        width_slice = slice(tpl[2], tpl[3])
+
+        img = img[height_slice, width_slice]
+
+        self._dump(img)
+
+        return find_subimages(img, self.image_to_find)
+
+    def _dump(self, img):
+        Detector.counter += 1
+        cv2.imwrite(f'output_{Detector.counter}.png', img)
+        return img
 
 
-class DetectMonitor:
-    def __init__(self):
-        self.monitors = {
-            path: cv2.imread(str(path), 0)
-            for path in Path('resources').glob('monitor*.png')
-        }
+class DetectGem(Detector):
+    IMAGE_TO_FIND = 'resources/gem_icon_visible.png'
 
-    def is_present(self, img):
-        img = cv2.imread(str(img), 0)
-        # Cut off the right bottom quadrant (speed up processing!)
-        img = img[img.shape[0]//2:, img.shape[1]//2:]
+    def _rescale(self, h, w):
+        # Top right part of the image
+        return 0, h // 6, w - w // 4, 0
 
-        for path, x in self.monitors.items():
-            res = find_subimages(img, x)
-            if res:
-                return res
 
-        return []
+class DetectMonitor(Detector):
+    IMAGE_TO_FIND = 'resources/monitor_no_thanks.png'
+
+    def _rescale(self, h, w):
+        half_of_height = h // 2
+        height_of_button = half_of_height + h // 8
+        half_of_width = w // 2
+
+        return half_of_height, height_of_button, 0, half_of_width
+
+
+class DetectGameFinished(Detector):
+    IMAGE_TO_FIND = 'resources/game_finished.png'
+
+    def _rescale(self, h, w):
+        return h // 2,
+
+
+class DetectMonsterSetup(Detector):
+    IMAGE_TO_FIND = 'resources/monster_setup.png'
+
+    def _rescale(self, h, w):
+        return h - h // 8
+
+
+class DetectSelectNewGame(Detector):
+    IMAGE_TO_FIND = 'resources/select_new_game.png'
+
+    def _rescale(self, h, w):
+        bottom_of_image = h // 4
+        height_of_button = h // 10
+
+        new_width = int(w / 2.5)
+
+        return h - bottom_of_image, h - bottom_of_image + height_of_button, 0, new_width
+
+
+class DetectSeller(Detector):
+    IMAGE_TO_FIND = 'resources/seller_no_thanks.png'
+
+    def _rescale(self, h, w):
+        half_of_height = h // 2
+        height_of_button = half_of_height + h // 8
+        half_of_width = w // 2
+
+        return half_of_height, height_of_button, 0, half_of_width
+
+
+class DetectSellerOkay(Detector):
+    IMAGE_TO_FIND = 'resources/seller_okay.png'
+
+    def _rescale(self, h, w):
+        bottom_of_image = h // 2
+        height_of_button = h // 8
+
+        return bottom_of_image, bottom_of_image + height_of_button
 
 
 if __name__ == '__main__':
     start = time()
     count = 0
 
-    tmp = DetectMonitor()
+    tmp = DetectSellerOkay()
 
-    for path in Path('../../tests/monitor/').rglob('*.png'):
+    for path in Path('../../tests/').rglob('*.png'):
+        if 'debug' in path.name: continue
+
         print(path, ' --> ', tmp.is_present(path))
         count += 1
     #
---
+++ src/summoners_greed_bot/find_window.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+from typing import Any, Tuple
+
+import win32con
+import win32gui
+
+
+class Window:
+    def __init__(self, hwnd: int):
+        self.hwnd = hwnd
+
+        rect = win32gui.GetWindowRect(hwnd)
+
+        self.x = rect[0]
+        self.y = rect[1]
+        self.w = rect[2] - rect[0]
+        self.h = rect[3] - rect[1]
+
+    def set_foreground(self):
+        """put the window in the foreground"""
+        win32gui.SetForegroundWindow(self.hwnd)
+
+    def un_minimize(self):
+        if win32gui.IsIconic(self.hwnd):
+            win32gui.ShowWindow(self.hwnd, win32con.SW_RESTORE)
+
+
+def find_window_with_title(title):
+    win = Window(hwnd=win32gui.FindWindow(None, title))
+    win.un_minimize()
+    win.set_foreground()
+    return win
+
+
+if __name__ == '__main__':
+    print(
+        find_window_with_title('BlueStacks')
+    )
